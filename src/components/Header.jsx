5b514e177f72179e98a3636d610ce6d7e8001db4
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-11-29 15:49:06 +0330

    #

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Contact App</h1>
+      <p><a href="google.com"></a></p>
+    </div>
+  );
+}
+
+export default Header;
