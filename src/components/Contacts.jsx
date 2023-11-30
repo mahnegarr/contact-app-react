8acc50ad287dd686425b39e05d1a8abc008a72eb
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-11-30 19:17:01 +0330

    #

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -20,34 +20,43 @@ function Contacts() {
 
   const addHandler = () => {
     setContacts((contacts) => [...contacts, contact]);
-    console.log(contacts);
+    setContact({
+      name: "",
+      lastName: "",
+      email: "",
+      phone: "",
+    });
   };
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-slate-400 h-[180px] rounded-[1rem]">
         <div className="grid grid-cols-2 gap-4 grid-rows-2 mb-4">
-          <input className="rounded-[.3rem] w-[430px] h-9 border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+          <input
+            className="rounded-[.3rem] w-[430px] h-9 border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
             type="text"
             placeholder="Name"
             name="name"
             value={contact.name}
             onChange={changeHandler}
           />
-          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+          <input
+            className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
             type="text"
             placeholder="Last Name"
             name="lastName"
             value={contact.lastName}
             onChange={changeHandler}
           />
-          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+          <input
+            className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
             type="text"
             placeholder="Email"
             name="email"
             value={contact.email}
             onChange={changeHandler}
           />
-          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+          <input
+            className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
             type="number"
             placeholder="Phone"
             name="phone"
@@ -56,8 +65,9 @@ function Contacts() {
           />
         </div>
         <div>
-
-        <button className="w-full bg-neutral-300" onClick={addHandler}>Add Contact</button>
+          <button className="w-full bg-neutral-300" onClick={addHandler}>
+            Add Contact
+          </button>
         </div>
       </div>
       <ContactsList contacts={contacts} />
