23a203c7d7a8ebd6eec715da02d428909ab7261b
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-11-30 19:14:09 +0330

    #

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -24,37 +24,41 @@ function Contacts() {
   };
   return (
     <>
-      <div className="flex items-center justify-center">
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={contact.name}
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value={contact.lastName}
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          placeholder="Email"
-          name="email"
-          value={contact.email}
-          onChange={changeHandler}
-        />
-        <input
-          type="number"
-          placeholder="Phone"
-          name="phone"
-          value={contact.phone}
-          onChange={changeHandler}
-        />
+      <div className="flex flex-col items-center justify-center bg-slate-400 h-[180px] rounded-[1rem]">
+        <div className="grid grid-cols-2 gap-4 grid-rows-2 mb-4">
+          <input className="rounded-[.3rem] w-[430px] h-9 border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={contact.name}
+            onChange={changeHandler}
+          />
+          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            value={contact.lastName}
+            onChange={changeHandler}
+          />
+          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={contact.email}
+            onChange={changeHandler}
+          />
+          <input className="rounded-[.3rem] border-[3px] hover:outline-none focus:outline-none focus:border-[#0a20a0] placeholder:pl-4"
+            type="number"
+            placeholder="Phone"
+            name="phone"
+            value={contact.phone}
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
 
-        <button onClick={addHandler}>Add Contact</button>
+        <button className="w-full bg-neutral-300" onClick={addHandler}>Add Contact</button>
+        </div>
       </div>
       <ContactsList contacts={contacts} />
     </>
