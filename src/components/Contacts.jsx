bbd2e68d41861bfbdfa38a5ad360fc4f4af9b29d
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-11-30 18:50:05 +0330

    #

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import ContactsList from "./ContactsList";
+
 function Contacts() {
   const [contacts, setContacts] = useState([]);
   const [contact, setContact] = useState({
@@ -21,38 +23,41 @@ function Contacts() {
     console.log(contacts);
   };
   return (
-    <div className="flex items-center justify-center">
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={contact.name}
-        onChange={changeHandler}
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        name="lastName"
-        value={contact.lastName}
-        onChange={changeHandler}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        name="email"
-        value={contact.email}
-        onChange={changeHandler}
-      />
-      <input
-        type="number"
-        placeholder="Phone"
-        name="phone"
-        value={contact.phone}
-        onChange={changeHandler}
-      />
+    <>
+      <div className="flex items-center justify-center">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={contact.lastName}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+        <input
+          type="number"
+          placeholder="Phone"
+          name="phone"
+          value={contact.phone}
+          onChange={changeHandler}
+        />
 
-      <button onClick={addHandler}>Add Contact</button>
-    </div>
+        <button onClick={addHandler}>Add Contact</button>
+      </div>
+      <ContactsList contacts={contacts} />
+    </>
   );
 }
 
