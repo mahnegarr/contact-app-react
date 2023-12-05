eba21d740ae3db65acaf3b93fc5641a13a10fce7
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-12-05 12:53:09 +0330

    #

--- src/components/ContactsBU.jsx
+++ src/components/ContactsBU.jsx
@@ -1,14 +1,57 @@
 import React, { useState } from "react";
 
 function ContactsBU() {
-    const contact = {} = useState()
+  const [contacts, setContacts] = useState([]);
+  const [contact, setContact] = useState({
+    id: "",
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+
+  const addHandler=()=>{
+    const newContact = {...contact , id:v4()}
+    setContacts((contacts)=>([...contacts, newContact]))
+  }
   return (
     <div>
       <div>
-        <input type="text" placeholder="name" name="name" />
-        <input type="text" placeholder="lastName" name="lastName" />
-        <input type="email" placeholder="email" name="email" />
-        <input type="number" placeholder="phone" name="phone" />
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          placeholder="lastName"
+          name="lastName"
+          value={contact.lastName}
+          onChange={changeHandler}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+        <input
+          type="number"
+          placeholder="phone"
+          name="phone"
+          value={contact.phone}
+          onChange={changeHandler}
+        />
       </div>
       <button>Add Contact</button>
     </div>
