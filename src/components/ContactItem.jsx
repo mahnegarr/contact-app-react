ac3316aa79525d29b22946c8b58cffa5985add0e
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2023-12-11 19:16:49 +0330

    #

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 
-function ContactItem(props) {
-    console.log(props);
+
+function ContactItem({data : {id,name,lastName,phone,email}}) {
+
   return (
     <li
       className="rounded-[8px] grid grid-cols-5 grid-rows-1 mb-3 p-3  bg-slate-200"
-      key={contact.id}
+      key={id}
     >
       <p className=" mt-1 ml-2">
-        {contact.name} {contact.lastName}
+        {name} {lastName}
       </p>
       <p className="flex col-span-2">
         <span className="bg-white rounded-[100%]  p-1 mr-2  ">📧</span>
-        <p className="mt-1">{contact.email} </p>
+        <p className="mt-1">{email} </p>
       </p>
       <p className="flex align-middle">
         <span className="bg-white rounded-[100%] p-1 mr-2 ">📞</span>
-        <p className=" mt-1">{contact.phone}</p>
+        <p className=" mt-1">{phone}</p>
       </p>
       <p>
         <span className="bg-white rounded-[50%] py-1 px-2 float-right mr-2 cursor-pointer ">
